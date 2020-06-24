f8aa9f0215969df3a2d06554f9d51af6b5cf90c6
tarwin3v <tarwin.ev@gmail.com>
2020-06-24 17:48:12 +0200

    conditionals

--- src/conditionals/Conditionals.kt
+++ src/conditionals/Conditionals.kt
@@ -1,15 +1,17 @@
 package conditionals
 
 fun main(args: Array<String>) {
-    val mode: Int = 4
-
-    when (mode) {
-        1 -> println("The mode is lazy.")
-        2 -> println("The mode is busy.")
-        3 -> {
-            println("The mode is super-productive.")
+    val x = 9
+    when(x){
+        5 -> println("x is 5.")
+        3*12 -> println("x is 3*12")
+        "Hey there".length -> {
+            println("x is the length of the string 'Hey there'")
         }
-        else -> println("What are you?")
+        in 1..10 -> println("x is between 1 & 10 ")
+        in 11..20 -> println("x is between 11 & 20 ")
+        !in 1..9 -> println("x is not between 1 & 9 ")
+
     }
 
 }
