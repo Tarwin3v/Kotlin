4d94f4710ce0a8f7d12be52a19e7f09395a4f612
tarwin3v <tarwin.ev@gmail.com>
2020-06-24 18:13:27 +0200

    conditionals

--- src/conditionals/Conditionals.kt
+++ src/conditionals/Conditionals.kt
@@ -1,17 +1,14 @@
 package conditionals
 
+import java.util.*
+
 fun main(args: Array<String>) {
-    val x = 9
-    when(x){
-        5 -> println("x is 5.")
-        3*12 -> println("x is 3*12")
-        "Hey there".length -> {
-            println("x is the length of the string 'Hey there'")
-        }
-        in 1..10 -> println("x is between 1 & 10 ")
-        in 11..20 -> println("x is between 11 & 20 ")
-        !in 1..9 -> println("x is not between 1 & 9 ")
-
+    val random = Random().nextInt(50) + 1
+    when(random){
+        in 1..10  -> println("random is between 1 & 10")
+        in 11..20 -> println("random is between 1 & 10")
+        in 21..30 -> println("random is between 1 & 10")
+        in 31..40 -> println("random is between 1 & 10")
+        else -> println("random is above 40")
     }
-
 }
