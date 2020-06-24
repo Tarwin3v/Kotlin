edf2535309f925ebe437449c0c48cc37840eaff2
tarwin3v <tarwin.ev@gmail.com>
2020-06-24 19:26:55 +0200

    loops

---
+++ src/loops/Loops.kt
@@ -0,0 +1,27 @@
+package loops
+
+fun main(args: Array<String>) {
+
+    var sum = 0
+
+    for ( i in 1..10) {
+        sum += i
+        println(sum)
+    }
+
+    val list = listOf("Java","Kotlin","Python")
+
+    for (element in list){
+        println(element)
+    }
+
+    for((index,value) in list.withIndex()) {
+        println("$index : $value")
+    }
+
+    var x = 10
+    while(x >= 0){
+        println(x)
+        x--
+    }
+}
